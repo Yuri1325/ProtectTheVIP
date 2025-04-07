903e34ff7bdb34015094382c6125bab44f626f77
Uri <uribrew21@gmail.com>
2025-04-07 10:33:52 -0400

    --

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 import pygame
+from Player import *
 
 pygame.init()
-screen = pygame.display.set_mode((1920,1088))
+screen = pygame.display.set_mode((1920/2,1088/2))
 clock = pygame.time.Clock()
-
+p = Player(True)
 run = True
 while run:
+    screen.fill((0,0,0))
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             run = False
-            
+    p.update(screen)
     pygame.display.update()
+    pygame.display.flip()
+    
 pygame.quit()
--- Player.py
+++ Player.py
@@ -4,4 +4,22 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,facingRight = True):
         super().__init__()
         self.position = vector(0,0)
-        
+        self.rect = pygame.Surface((20,50))
+        self.rect.fill('White')
+   
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.position[1] -=5
+        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.position[1] +=5
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.position[0] +=5
+        elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.position[0] -=5
+            
+    def update(self,win):
+        self.draw(win)
+        self.movement()
+    def draw(self,window):
+        window.blit(self.rect,(self.position[0],self.position[1]))
