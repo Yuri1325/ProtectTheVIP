3c65db4db41a648452dfc74ddc92a8382e289b37
Uri <uribrew21@gmail.com>
2025-04-09 08:24:17 -0400

    ---

--- Main.py
+++ Main.py
@@ -17,16 +17,17 @@ def sortPackages(wanted_id,list):
     for x in list:
         print(type(x.getPlayer()))
         if x.getClient_Id == wanted_id:
-            return x.getPlayer()
+            if not(x.getPlayer()==None):
+                return x.player
 def handleOtherPalyer(otherPackage):
     global playersList
-    otherPlayers_1 =sortPackages,(1,otherPackage)
-    otherPlayers_2 = sortPackages,(2,otherPackage)
-    otherPlayers_3 = sortPackages,(3,otherPackage)
-    print(type(otherPlayers_1))
-    #otherPlayers_1.update(screen)
-    #otherPlayers_2.update(screen)
-    #otherPlayers_3.update(screen)
+    otherPlayers_1 =sortPackages(1,otherPackage)
+    otherPlayers_2 = sortPackages(2,otherPackage)
+    otherPlayers_3 = sortPackages(3,otherPackage)
+    
+    otherPlayers_1.update(screen)
+    otherPlayers_2.update(screen)
+    otherPlayers_3.update(screen)
     
 
 while run:
