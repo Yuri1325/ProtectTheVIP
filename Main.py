747437fd0d9ff7b5dc051fb6490f2fd743487f43
Uri <uribrew21@gmail.com>
2025-04-10 10:59:04 -0400

    ---

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Player import *
 from Network import *
 from Package import *
 from _thread import *
-import VaribleWatcher 
+
 pygame.init()
 screen = pygame.display.set_mode((1920/2,1088/2))
 clock = pygame.time.Clock()
@@ -14,24 +14,13 @@ package = Package(n.id,player)
 playersList =[]
 run = True
 
-def sortPackages(wanted_id,list):
-    for x in list:
-        
-        if x.getPlayer().player_id == wanted_id:
-                return x.getPlayer()
-        
-def handleOtherPalyer(otherPackage):
-    global playersList
-    otherPlayers_1 = sortPackages(1,otherPackage)
-    otherPlayers_2 = sortPackages(2,otherPackage)
-    otherPlayers_3 = sortPackages(3,otherPackage)
-    return otherPlayers_1,otherPlayers_2,otherPlayers_3
 
 
-def displayOtherCharacter():
-     for i in playersList:
-         if (i!=None):
-            i.update(screen)
+
+# def displayOtherCharacter():
+#      for i in playersList:
+#          if (i!=None):
+#             i.update(screen)
     
     
    
@@ -51,7 +40,6 @@ while run:
     package.setPlayer(player)
     otherPackages = n.send(package)
     p1,p2,p3 = handleOtherPalyer(otherPackages)
-    #vw = VaribleWatcher.VaribleWatcher("Player Position",player.position)
     
     
     
